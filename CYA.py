5e4b5cf22c089a6897249979a8136372ed1b36ef
thatKevinSmall <thatKevinSmall@gmail.com>
2025-01-01 16:08:10 -0600

    Update CYA.py

--- CYA.py
+++ CYA.py
@@ -1,12 +1,9 @@
-import os
-import sys
-import time
+import os, sys, time
 
 class Chapter:
     def __init__(self, chapterText, isEnding):
         self.chapterText = chapterText
         self.choices = []
-        self.choicesIndex = []
         self.isEnding = isEnding
     
     def addChoice(self, choice):
@@ -23,8 +20,9 @@ class Chapter:
             
     def validateChoice(self, idx):
         try:
-            if self.choices[idx - 1]:
-                return self.choices[idx - 1].goto
+            idx = int(idx) - 1
+            if self.choices[idx]:
+                return self.choices[idx].goto
         except:
             return None
         
@@ -39,18 +37,11 @@ def delayPrint(someText):
         sys.stdout.flush()
         time.sleep(.05)
 
-def isInteger(value):
-    try:
-        int(value)
-        return True
-    except ValueError:
-        return False
-
 #create chapters passing chapter text and true or false is it an ending
 chapter1 = Chapter("You are in a clearing.\nYou are all alone.\n", False)
 chapter2 = Chapter("You are back home.\nYou climb into bed.\n", True)
-chapter3 = Chapter("You scroung around for wood to make a fire.\nYou come accross a snake.\n", False)
-chapter4 = Chapter("You swing a stick at the snake.\n  It bites you and it was poisonous.\nYou die.", True)
+chapter3 = Chapter("You scrounge around for wood to make a fire.\nYou come across a snake.\n", False)
+chapter4 = Chapter("You swing a stick at the snake.\nIt bites you and it was poisonous.\nYou die.\n", True)
 
 #create choices passing in the choice text and the chpater it goes to
 ChoiceGoHome = Choice("You can run back home.\n", chapter2)
@@ -66,26 +57,23 @@ chapter3.addChoice(choiceAttackSnake)
 gameContinue = True
 currentChapter = chapter1
 
+#while loop
 while gameContinue:
     if not currentChapter.isEnding:
         currentChapter.printChapter()
         userInput = input("Enter the number of your choice.\n")
-        #verify the choice is a number
-        if isInteger(userInput):
-            response = currentChapter.validateChoice(int(userInput))
+        response = currentChapter.validateChoice(userInput)
             #verify the choice is in this chapter
-            if response:
+        if response:
                     currentChapter = response
-            else:
+        else:
                 delayPrint(f"{userInput} is invalid choice.\n")
                 
-        else:
-            delayPrint(f"{userInput} is invalid choice.\n")
-
+        
     else:
         delayPrint(currentChapter.chapterText)
         delayPrint("Game is over.\nWould you like to try again?\n")
-        userInput = input("Y or N")
+        userInput = input("Y or N\n")
         if userInput.lower() == "y":
             currentChapter = chapter1
         elif userInput.lower() == "n":
